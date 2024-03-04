0afcd79169e1335087be1ac5377cb3d6fac56861
ALAGAPATIBHARATH <162090338+ALAGAPATIBHARATH@users.noreply.github.com>
2024-03-04 11:36:28 +0530

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,23 @@
+let celsiusEl=document.getElementById("celsius")
+let fahrenheitEl=document.getElementById("fahrenheit")
+let kelvinEl=document.getElementById("kelvin")
+function computeTemp(event){
+    let currentValue=+event.target.value
+
+    switch (event.target.name) {
+        case "celsius":
+            kelvinEl.value=(currentValue+273.15).toFixed(2)
+            fahrenheitEl.value=(currentValue*1.8 + 32).toFixed(2)
+            break;
+        case "fahrenheit":
+            celsiusEl.value=((currentValue -32)/1.8).toFixed(2)
+            kelvinEl.value=((currentValue-32)/1.8+273.15).toFixed(2)
+            break;
+        case "kelvin":
+            celsiusEl.value=(currentValue -273.32).toFixed(2)
+            fahrenheitEl.value=((currentValue-273.15)*1.8+32).toFixed(2)
+            break;    
+        default:
+            break;
+    }
+}
